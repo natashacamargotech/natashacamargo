7e08cf253a7d126ccee0d372baad108c847910a4
natashacamargo89 <natashacamargo89@gmail.com>
2024-03-02 11:34:39 -0300

    Atualizacao do projeto

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-layout view="lHh lpr lFf" class="shadow-2 rounded-borders">
+    <q-header>
+      <q-toolbar class="bg-pink">
+        <q-toolbar-title>Natasha Camargo</q-toolbar-title>
+
+        <q-tabs v-model="tab">
+          <q-tab name="sobre" label="Sobre mim" />
+          <q-tab name="trabalhos" label="Projetos" />
+          <q-tab name="skills" label="Skills" />
+          <q-tab name="contato" label="Contato" />
+        </q-tabs>
+
+        <q-space />
+      </q-toolbar>
+    </q-header>
+
+    <q-footer>
+      <q-toolbar class="bg-pink">
+        <q-toolbar-title class="text-center">
+          Copyright © 2024 | Criado por Natasha Camargo
+          <q-space />
+          <q-btn
+            dense
+            flat
+            icon="whatshot"
+            style="font-size: 15px; color: white"
+          />
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+<script setup></script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,206 @@
+<template>
+  <img src="public/img/banner.png" />
+
+  <div class="q-pa-xl row items-center" href="#sobre">
+    <div class="col-7">
+      <q-card class="q-pa-md bg-pink text-h5 text-white">
+        <h4 class="text-center">Sobre Mim</h4>
+        <p class="text-justify-between">
+          Sou uma profissional com experiência em Odontologia e estou em
+          transição de carreira para a área de Análise e Desenvolvimento de
+          Sistemas.
+        </p>
+        <p class="text-justify-between">
+          Minha atuação como cirurgiã-dentista com vivência nas áreas de
+          elaboração de diagnósticos e atendimento ao cliente contribuem para a
+          minha facilidade em implementar, desenvolver e projetar sistemas.
+          Tenho experiência em docência e consultório, trabalho em equipe,
+          liderança de equipe, capacidade analítica e de resolução de problemas,
+          organização e gestão de projetos.
+        </p>
+        <p class="text-justify-between">
+          Atualmente sou desenvolvedora de sistemas - FullStack em projetos de
+          acesso corporativo e público.
+        </p>
+      </q-card>
+    </div>
+    <div class="col-5 q-mx-sx">
+      <q-img src="public/img/natashaperfil.png" width="450px" height="500px" />
+    </div>
+  </div>
+  <div class="q-col-gutter-md row items-star bg-red" href="#trabalhos">
+    <h1 vclass="text-center">Hard Skilss</h1>
+  </div>
+  <div class="q-pa-md" style="max-width: 350px">
+    <q-list dense bordered padding class="rounded-borders">
+      <q-item clickable v-ripple>
+        <q-item-section class="text-h6 text-center"> Frontend </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section class="text-center"> HTML </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section class="text-center"> CSS </q-item-section>
+      </q-item>
+
+      <q-item clickable v-ripple>
+        <q-item-section class="text-center"> JavaScript </q-item-section>
+      </q-item>
+    </q-list>
+    <div class="q-pa-md" style="max-width: 350px">
+      <q-list dense bordered padding class="rounded-borders">
+        <q-item clickable v-ripple>
+          <q-item-section class="text-h6 text-center"> Backend </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section class="text-center"> Vue </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section class="text-center"> JAVA </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section class="text-center"> Flutter </q-item-section>
+        </q-item>
+
+        <q-item clickable v-ripple>
+          <q-item-section class="text-center"> Angular </q-item-section>
+        </q-item>
+      </q-list>
+      <div class="q-pa-md" style="max-width: 350px">
+        <q-list dense bordered padding class="rounded-borders">
+          <q-item clickable v-ripple>
+            <q-item-section class="text-h6 text-center">
+              Frameworks
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section class="text-center"> Quasar </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section class="text-center"> Bootstrap </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section class="text-center"> Springboot </q-item-section>
+          </q-item>
+        </q-list>
+        <div class="q-pa-md" style="max-width: 350px">
+          <q-list dense bordered padding class="rounded-borders">
+            <q-item clickable v-ripple>
+              <q-item-section class="text-h6 text-center">
+                Testes
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section class="text-center"> Selenium </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section class="text-center"> Play Wright </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+        <div class="q-pa-md" style="max-width: 350px">
+          <q-list dense bordered padding class="rounded-borders">
+            <q-item clickable v-ripple>
+              <q-item-section class="text-h6 text-center">
+                Banco de Dados
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section class="text-center"> Oracle </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section class="text-center">
+                SQL DEVELOPER
+              </q-item-section>
+            </q-item>
+          </q-list>
+          <div class="q-pa-md" style="max-width: 350px">
+            <q-list dense bordered padding class="rounded-borders">
+              <q-item clickable v-ripple>
+                <q-item-section class="text-h6 text-center">
+                  Serviços de aplicação:
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-ripple>
+                <q-item-section class="text-center"> Widfly </q-item-section>
+              </q-item>
+
+              <q-item clickable v-ripple>
+                <q-item-section class="text-center"> JBOSS </q-item-section>
+              </q-item>
+            </q-list>
+          </div>
+          <div class="q-pa-md" style="max-width: 350px">
+            <q-list dense bordered padding class="rounded-borders">
+              <q-item clickable v-ripple>
+                <q-item-section class="text-h6 text-center">
+                  Gerenciamento
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-ripple>
+                <q-item-section class="text-center"> TRELLO </q-item-section>
+              </q-item>
+
+              <q-item clickable v-ripple>
+                <q-item-section class="text-center">
+                  GLPI(Chamados)
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </div>
+          <div class="q-pa-md" style="max-width: 350px">
+            <q-list dense bordered padding class="rounded-borders">
+              <q-item clickable v-ripple>
+                <q-item-section class="text-h6 text-center">
+                  Outros
+                </q-item-section>
+              </q-item>
+
+              <q-item clickable v-ripple>
+                <q-item-section class="text-center"> GitHub </q-item-section>
+              </q-item>
+
+              <q-item clickable v-ripple>
+                <q-item-section class="text-center"> GitLab </q-item-section>
+              </q-item>
+            </q-list>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="q-col-gutter-md row items-star bg-orange" href="#trabalhos">
+    <h1 vclass="text-center">Soft Skills</h1>
+  </div>
+  <div class="q-col-gutter-md row items-star bg-red" href="#trabalhos">
+    <h1 vclass="text-center">Projetos</h1>
+  </div>
+
+  <h1 class="text-center">Contato</h1>
+  <div class="q-col-gutter-md row items-star bg-orange" href="#trabalhos">
+    <div class="q-col-gutter-md q-pa-xl">
+      <q-btn class="glossy" color="pink" icon="X" label="GitHub" />
+    </div>
+    <div class="q-col-gutter-md q-pa-xl">
+      <q-btn class="glossy" color="pink" icon="X" label="LinkedIn" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+</script>
